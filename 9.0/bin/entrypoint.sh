01ef0afde3d42f5436f6fddd0efba3bd54a51f36
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-02-22 17:00:12 +0100

    Always call dockerize -template
    So if we call another bin, such as runtests, we have a proper
    configuration file

--- 9.0/bin/entrypoint.sh
+++ 9.0/bin/entrypoint.sh
@@ -2,15 +2,17 @@
 set -e
 
 BASEDIR=$(dirname $0)
-$BASEDIR/wait_postgres.sh
 
-if [ "$1" = "./src/odoo.py" ]; then
+CONFIGDIR=$BASEDIR/..
+if [ -e $CONFIGDIR/etc/openerp.cfg.tmpl ]; then
+  dockerize -template $CONFIGDIR/etc/openerp.cfg.tmpl:$CONFIGDIR/etc/openerp.cfg
+fi
+
+$(dirname $0)/wait_postgres.sh
+
+if [ "$1" = "./src/odoo.py" ] || [ "$1" = "src/odoo.py" ]; then
   chown -R odoo .
   chown -R odoo /data/odoo
-
-  if [ -e etc/openerp.cfg.tmpl ]; then
-    dockerize -template etc/openerp.cfg.tmpl:etc/openerp.cfg
-  fi
 
   gosu odoo bin/migrate
   exec gosu odoo "$@"
