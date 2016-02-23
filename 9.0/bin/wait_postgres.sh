28045dce7055758bbac2cdec28b305a46df39e84
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-02-23 16:14:28 +0100

    Wait a little bit more after postgres'port is open

--- 9.0/bin/wait_postgres.sh
+++ 9.0/bin/wait_postgres.sh
@@ -12,3 +12,7 @@ if [ -z $DB_PORT ]; then
   DB_PORT=5432
 fi
 dockerize -wait tcp://db:${DB_PORT}
+# port is up before postgres is totally ready:
+# 'createdb: could not connect to database template1: FATAL:  the database system is starting up'
+# wait a little bit more
+sleep 1
