f66eda24e58126f5936f7dfe55e54f8c4cfb9a6c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-05-10 15:29:36 +0200

    Set default value for DB_PORT

--- 9.0/bin/wait_postgres.sh
+++ 9.0/bin/wait_postgres.sh
@@ -7,10 +7,6 @@ set -e
 BASEDIR=$(dirname $0)
 CONFIGDIR=$BASEDIR/../etc
 
-DB_PORT=$(grep db_port $CONFIGDIR/openerp.cfg | cut -d ' ' -f 3)
-if [ -z $DB_PORT ]; then
-  DB_PORT=5432
-fi
 dockerize -wait tcp://db:${DB_PORT}
 # port is up before postgres is totally ready:
 # 'createdb: could not connect to database template1: FATAL:  the database system is starting up'
