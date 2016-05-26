1130e89e7b75830e7d926a61463a905d931f0a06
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-05-26 10:50:37 +0200

    Wait postgres 30 seconds
    Seems that 10 seconds is insufficient (saw that on a travis build)
    Anyway it doesn't hurt to wait a bit more.

--- 9.0/bin/wait_postgres.sh
+++ 9.0/bin/wait_postgres.sh
@@ -7,8 +7,8 @@ set -e
 BASEDIR=$(dirname $0)
 CONFIGDIR=$BASEDIR/../etc
 
-dockerize -wait tcp://db:${DB_PORT}
+dockerize -timeout 30s -wait tcp://db:${DB_PORT}
 # port is up before postgres is totally ready:
 # 'createdb: could not connect to database template1: FATAL:  the database system is starting up'
 # wait a little bit more
-sleep 1
+sleep 2
