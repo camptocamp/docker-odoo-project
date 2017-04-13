2a2b37ea0e4d98d6ab06200694d0396fbda04f1c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-04-13 13:49:57 +0200

    Possibility to customize the db hostname
    Default is still db, but it can be changed to avoid to add an external
    link.
    
    Alternative of https://github.com/camptocamp/docker-odoo-project/pull/17

--- bin/wait_postgres.sh
+++ bin/wait_postgres.sh
@@ -7,13 +7,13 @@ set -e
 BASEDIR=$(dirname $0)
 CONFIGDIR=$BASEDIR/../etc
 
-dockerize -timeout 30s -wait tcp://db:${DB_PORT}
+dockerize -timeout 30s -wait tcp://${DB_HOST}:${DB_PORT}
 
 # now the port is up but sometimes postgres is not totally ready yet:
 # 'createdb: could not connect to database template1: FATAL:  the database system is starting up'
 # we retry if we get this error
 
-while [ "$(PGPASSWORD=$DB_PASSWORD psql -h db -U $DB_USER -c '' postgres 2>&1)" = "psql: FATAL:  the database system is starting up" ]
+while [ "$(PGPASSWORD=$DB_PASSWORD psql -h ${DB_HOST} -U $DB_USER -c '' postgres 2>&1)" = "psql: FATAL:  the database system is starting up" ]
 do
   echo "Waiting for the database system to start up"
   sleep 0.1
