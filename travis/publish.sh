b5c658af2674cdf30d716826727e41fac27fd534
Sébastien BEAU <sebastien.beau@akretion.com>
2018-06-20 14:19:24 +0200

    Refactor, do not use matrice for building full and light image, but instead base the full image on the light one. With that solution we save the plannet (less job, less electricity consommation) and we also reduce the size needed for all images (layer are shared) and avoid to have difference between image

--- travis/publish.sh
+++ travis/publish.sh
@@ -7,11 +7,11 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     if [ "$VERSION" == "10.0" ]; then
-      make VERSION=$VERSION TAG=$TRAVIS_TAG tag_latest_main push_latest_main
+      make VERSION=$VERSION TAG=$TRAVIS_TAG FULL=$FULL tag_latest_main push_latest_main
     fi
-    make VERSION=$VERSION TAG=latest tag push
+    make VERSION=$VERSION TAG=latest FULL=$FULL tag push
   elif [ ! -z "$TRAVIS_TAG" ]; then
-    make VERSION=$VERSION TAG=$TRAVIS_TAG tag push
+    make VERSION=$VERSION TAG=$TRAVIS_TAG FULL=$FULL tag push
   else
     echo "Not pushing any image"
   fi
