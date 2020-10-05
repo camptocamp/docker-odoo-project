960b06cdcfaf57b5ec09361a38183c78ba5a6bf3
Denis Leemann <denis.leemann@camptocamp.com>
2020-10-05 13:38:47 +0200

    Update Dockerfile to 14.0 and travis base build branch

--- travis/publish.sh
+++ travis/publish.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 
   if [ "$TRAVIS_BRANCH" == "master" ]; then
-    if [ "$VERSION" == "13.0" ]; then
+    if [ "$VERSION" == "14.0" ]; then
       make VERSION=$VERSION TAG=$TRAVIS_TAG tag_latest_main push_latest_main
     fi
     make VERSION=$VERSION TAG=latest tag push
