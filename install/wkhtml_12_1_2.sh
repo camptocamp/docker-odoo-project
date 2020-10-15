5f62644f3186dc1a2a2bb076c199860cadd142fd
Patrick Tombez <patrick.tombez@camptocamp.com>
2020-10-15 12:42:55 +0200

    Fix wkhtmltopdf version for Odoo version <= 10

--- install/wkhtml_12_1_2.sh
+++ install/wkhtml_12_1_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-curl -o wkhtmltox.deb -SL http://nightly.odoo.com/extra/wkhtmltox-0.12.1.2_linux-jessie-amd64.deb
-echo '40e8b906de658a2221b15e4e8cd82565a47d7ee8 wkhtmltox.deb' | sha1sum -c -
-dpkg --force-depends -i wkhtmltox.deb
-rm wkhtmltox.deb
+# curl -o wkhtmltox.deb -SL http://nightly.odoo.com/extra/wkhtmltox-0.12.1.2_linux-jessie-amd64.deb
+echo 'bd86f4cc9bf7a515ba038ddb8b60eaffc1c4e2b9 /install/wkhtmltox.deb' | sha1sum -c -
+dpkg --force-depends -i /install/wkhtmltox.deb
+rm /install/wkhtmltox.deb
