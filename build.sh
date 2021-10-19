622b01a70174ac14fbbd15113b341b27374cb8a8
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2021-10-19 09:39:59 +0200

    minimal apt install ghostscript

--- build.sh
+++ build.sh
@@ -44,6 +44,6 @@ cp -r start-entrypoint.d/ ${TMP}
 cp -r before-migrate-entrypoint.d/ ${TMP}
 
 docker build --no-cache -f ${TMP}/Dockerfile -t ${IMAGE_LATEST} ${TMP}
-docker build --no-cache -f ${TMP}/Dockerfile-onbuild -t ${IMAGE_LATEST}-onbuild ${TMP}
-docker build --no-cache -f ${TMP}/Dockerfile-batteries -t ${IMAGE_LATEST}-batteries ${TMP}
-docker build --no-cache -f ${TMP}/Dockerfile-batteries-onbuild -t ${IMAGE_LATEST}-batteries-onbuild ${TMP}
+#docker build --no-cache -f ${TMP}/Dockerfile-onbuild -t ${IMAGE_LATEST}-onbuild ${TMP}
+#docker build --no-cache -f ${TMP}/Dockerfile-batteries -t ${IMAGE_LATEST}-batteries ${TMP}
+#docker build --no-cache -f ${TMP}/Dockerfile-batteries-onbuild -t ${IMAGE_LATEST}-batteries-onbuild ${TMP}
--- install/package_odoo-bullseye.sh
+++ install/package_odoo-bullseye.sh
@@ -3,24 +3,4 @@ set -eo pipefail
 
 apt-get update -o Acquire::AllowInsecureRepositories=true
 apt-get install -y --no-install-recommends \
-    antiword \
-    ca-certificates \
-    curl \
-    dirmngr \
     ghostscript \
-    graphviz \
-    gnupg2 \
-    less \
-    nano \
-    node-clean-css \
-    node-less \
-    poppler-utils \
-    python3 \
-    python3-pip \
-    python3-setuptools \
-    python3-renderpm \
-    libxslt1.1 \
-    xfonts-75dpi \
-    xfonts-base \
-    xz-utils \
-    tcl expect
