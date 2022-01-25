627c7b4d092d7b5a859fd015f3ca0195584b165e
vrenaville <vincent.renaville@camptocamp.com>
2022-01-25 12:06:43 +0100

    fix: switch to wkhtmltopdf

--- install/kwkhtml_client.sh
+++ install/kwkhtml_client.sh
@@ -3,6 +3,4 @@ set -eo pipefail
 
 curl -o kwkhtmltopdf_client -SL https://raw.githubusercontent.com/camptocamp/kwkhtmltopdf/master/client/python/kwkhtmltopdf_client.py
 echo '8676b798f57c67e3a801caad4c91368929c427ce kwkhtmltopdf_client' | sha1sum -c -
-sed -i "1 s/python/python3/g" /usr/local/bin/wkhtmltopdf
-cp kwkhtmltopdf_client /usr/local/bin/wkhtmltopdf
-rm -rf kwkhtmltopdf_client
+mv kwkhtmltopdf_client /usr/local/bin/wkhtmltopdf
