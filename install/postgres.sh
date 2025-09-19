08b5f73d9e67e2ca8c16a367b46eb19b76b97881
vrenaville <vincent.renaville@camptocamp.com>
2025-09-19 11:42:07 +0200

    fix:push to trixie

--- install/postgres.sh
+++ install/postgres.sh
@@ -3,23 +3,12 @@ set -eo pipefail
 
 OS_CODENAME=$(awk -F= '$1=="VERSION_CODENAME" { print $2 ;}' /etc/os-release)
 
-if [ -z "$OS_CODENAME" ]
-then
-    # VERSION_CODENAME doesn't exist in jessie
-    # To remove on drop of support of jessie
-    OS_CODENAME="jessie"
-fi
-
 
 APT_REPO="apt.postgresql.org"
-if [ $OS_CODENAME = "jessie" ] || [ $OS_CODENAME = "stretch" ]
-then
-    APT_REPO="apt-archive.postgresql.org"
-fi
 
-echo "deb http://${APT_REPO}/pub/repos/apt/ ${OS_CODENAME}-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+echo "deb  [arch=amd64 signed-by=/etc/apt/keyrings/postgresql.asc] http://${APT_REPO}/pub/repos/apt/ ${OS_CODENAME}-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc -o /etc/apt/keyrings/postgresql.asc
 
 apt-get update
-apt-get install -y --no-install-recommends postgresql-client libpq-dev
+apt-get install -y --no-install-recommends postgresql-client-14 libpq-dev
 apt-get -y install -f --no-install-recommends
