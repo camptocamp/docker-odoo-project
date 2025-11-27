ba3a9e305a3a9598a8dad0798188acbae0325886
Florent Xicluna <florent.xicluna@camptocamp.com>
2025-11-27 13:29:43 +0100

    feat: update link to Pip documentation

--- install/setup-pip.sh
+++ install/setup-pip.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eo pipefail
-# Install pip with ensuire pip
+# Install pip -- https://docs.python.org/library/ensurepip
 python -m ensurepip
 cat << EOF > /etc/pip.conf
 [global]
 # counter-intuitively, false means that we enable 'no-cache-dir'
 # "To enable the boolean options --no-compile and --no-cache-dir, falsy values have to be used"
-# https://pip.pypa.io/en/stable/user_guide/#configuration
+# https://pip.pypa.io/en/stable/topics/configuration/#boolean-options
 no-cache-dir = false
-disable-pip-version-check = True
+disable-pip-version-check = true
 EOF
