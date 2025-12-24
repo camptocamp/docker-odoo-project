63075093d7b4aabd6c097f14e9d9d052b47ce544
Florent Xicluna <florent.xicluna@camptocamp.com>
2025-12-24 07:54:37 +0100

    feat: improve install scripts, related to permissions

--- install/kwkhtml_client.sh
+++ install/kwkhtml_client.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 curl -o kwkhtmltopdf_client -SL https://raw.githubusercontent.com/camptocamp/kwkhtmltopdf/master/client/python/kwkhtmltopdf_client.py
 echo '8676b798f57c67e3a801caad4c91368929c427ce kwkhtmltopdf_client' | sha1sum -c -
 mv kwkhtmltopdf_client /usr/local/bin/wkhtmltopdf
-chmod a+x /usr/local/bin/wkhtmltopdf
+chmod ug+x /usr/local/bin/wkhtmltopdf
