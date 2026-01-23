0cdb9acfb031797447735ea017fd4ea3cd883717
Florent Xicluna <florent.xicluna@camptocamp.com>
2026-01-23 14:05:14 +0100

    Remove vulnerability detected in vendored dependency of setuptools

--- install/purge_dev_package_and_cache.sh
+++ install/purge_dev_package_and_cache.sh
@@ -3,3 +3,7 @@
 apt-get remove -y $BUILD_PACKAGE
 apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false -o APT::AutoRemove::SuggestsImportant=false $PURGE_PACKAGE
 rm -rf /var/lib/apt/lists/* /root/.cache/pip/*
+
+# Remove METADATA of vendored dependencies
+# which triggered a vulnerability report, without easy way to fix it.
+rm -vrf /odoo/.venv/lib/python3*/site-packages/setuptools/_vendor/*.dist-info
