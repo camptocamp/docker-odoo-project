4c52693250dbcb04c04950cb99a22f1ae8ccab23
Florent Xicluna <florent.xicluna@camptocamp.com>
2026-08-27 18:42:14 +0200

    fix: psql already read values PGHOST/PGUSER from environment
    Also remove redundant calls to wait_postgres.sh, since it is already in the common entrypoint

--- bin/env_postgres.sh
+++ bin/env_postgres.sh
@@ -1,5 +1,5 @@
 
-if [ ${DB_USER-} ]; then
+if [ -z "${PGUSER-}" ] && [ -n "${DB_USER-}" ]; then
   export PGHOST=$DB_HOST
   export PGPORT=$DB_PORT
   export PGUSER=$DB_USER
@@ -10,3 +10,23 @@ if [ ${DB_USER-} ]; then
     export PGPASSWORD=$DB_PASSWORD
   fi
 fi
+
+wait_postgres () {
+  TIMEOUT="${1:-30s}"
+  # Wait until PostgreSQL is running.
+  if [ -d "$DB_HOST" ]; then
+      dockerize -timeout $TIMEOUT -wait "unix://${DB_HOST}/.s.PGSQL.${DB_PORT}"
+  else
+      dockerize -timeout $TIMEOUT -wait "tcp://${DB_HOST}:${DB_PORT}"
+  fi
+
+  # now the port is up but sometimes server is not ready yet:
+  # 'createdb: could not connect to database template1: FATAL:  the database system is starting up'
+  # we retry if we get this error
+
+  while [ "$(psql -c '' postgres 2>&1)" = "psql: FATAL:  the database system is starting up" ]
+  do
+    echo "Waiting for the database system to start up"
+    sleep 0.1
+  done
+}
