5be750ca3739cf85da7abfd1598dc6ae55e6ed38
Florent Xicluna <florent.xicluna@camptocamp.com>
2026-09-11 16:22:03 +0200

    fix: remove error message when running testdb-gen/testdb-update

--- common/env_odoo.sh
+++ common/env_odoo.sh
@@ -8,7 +8,7 @@ USER_ID=${LOCAL_USER_ID:-999}
 id -u odoo &> /dev/null || useradd --shell /bin/bash -u $USER_ID -o -c "" -m odoo
 
 # If run as "gosu odoo ...", shift arguments
-if [ "$(basename "${1-}")" = "gosu" ]; then
+if [ "$(basename "./${1-}")" = "gosu" ]; then
   test "${2-}" = odoo
   shift 2
 fi
